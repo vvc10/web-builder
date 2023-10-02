965e74a94073ee729cc2c26e13738a5b3c3d4118
Pankaj Yadav <vvastcore@gmail.com>
2023-10-02 22:38:17 +0530

    commiting website buider!

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const editor = grapesjs.init({
+  container: "#editor",
+  fromElement: true,
+  width: "auto",
+  storageManager: false,
+  plugins: ["gjs-preset-webpage"],
+  pluginsOpts: {
+    "gjs-preset-webpage": {},
+  },
+});
+ 
